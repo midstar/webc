fede23b4f441c6f6bb5eb13d467ba5a115b8b7b0
Joel Midstjärna <joel.midstjarna@gmail.com>
2019-10-05 08:28:09 +0200

    Added source and README

---
+++ README.md
@@ -0,0 +1,42 @@
+# webc - One file command line WEB server
+
+[![AppVeyor](https://ci.appveyor.com/api/projects/status/github/midstar/webc?svg=true)](https://ci.appveyor.com/api/projects/status/github/midstar/webc)
+
+webc is a mini command line WEB server. It consists of one executable 
+and no external dependencies.
+
+All majort platforms supported such as Windows, Linux (x86 and ARM)
+and Mac.
+
+No installation required, just download the suitable executable 
+[here on GitHub](https://github.com/midstar/webc/releases). Or build
+the software yourself according to the instructions below.
+
+## Usage
+
+Copy the webc to a directory of your choice and add it to your PATH
+environment variable.
+
+    Usage: webc.exe [options] [<path>]
+    
+    <path> is the directory where your web files are located.
+    Default is current directory.
+    
+    Supported options:
+      -p int
+            Network port to listen to (default 8080)
+      -v    Display version
+
+## Build from source (any platform)
+
+To build from source on any platform you need to:
+
+* Install Golang 
+* Set the GOPATH environment variable
+
+    go get github.com/midstar/webc
+    go install github.com/midstar/webc
+
+## Author and license
+
+This application is written by Joel Midstjärna and is licensed under the MIT License.
---
+++ webc.go
@@ -0,0 +1,60 @@
+// webc is a minimal command line web server utility.
+//
+// Author: Joel Midstjärna
+// License: MIT
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+// Below globals are automatically updated by the CI by providing
+// -X linker flags while building
+var applicationVersion = "<NOT SET>"
+var applicationBuildTime = "<NOT SET>"
+var applicationGitHash = "<NOT SET>"
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options] [<path>]\n\n", os.Args[0])
+	fmt.Printf("<path> is the directory where your web files are located.\n")
+	fmt.Printf("Default is current directory.\n\n")
+	fmt.Printf("Supported options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = printUsage
+	var version = flag.Bool("v", false, "Display version")
+	var port = flag.Int("p", 8080, "Network port to listen to")
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("Version:    %s\n", applicationVersion)
+		fmt.Printf("Build Time: %s\n", applicationBuildTime)
+		fmt.Printf("GIT Hash:   %s\n", applicationGitHash)
+		os.Exit(0)
+	}
+
+	directory := "."
+	if flag.NArg() == 1 {
+		directory = flag.Arg(0)
+	} else if flag.NArg() > 1 {
+		fmt.Fprintf(os.Stderr, "Invalid number of arguments!\n\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fs := http.FileServer(http.Dir(directory))
+	http.Handle("/", fs)
+
+	fmt.Printf("Serving path %s on port %d\n", directory, *port)
+
+	err := http.ListenAndServe(":"+strconv.Itoa(*port), nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error! %s\n", err)
+	}
+}
